85bb7cc700eda7bcea5522d0cffd67f22d9b1a2c
Olatunji Omisakin <msknolatunji@gmail.com>
2021-12-07 13:06:20 +0100

    correct inconsistent paragraphs geberating

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React, { useState } from 'react';
 import data from './data';
-
 function App() {
   const [count, setCount] = useState(0);
-  const [texts, setText] = useState([]);
+  const [text, setText] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    let amount = +count;
+    let amount = parseInt(count);
     if (count <= 0) {
       amount = 1;
     }
-    if (count > texts.length) {
+    if (count > 8) {
       amount = 8;
     }
     setText(data.slice(0, amount));
   };
   return (
     <section className='section-center'>
-      <h3>tired of boring lorem ipsum</h3>
+      <h3>tired of boring lorem ipsum?</h3>
       <form className='lorem-form' onSubmit={handleSubmit}>
-        <label htmlFor='amount'>password:</label>
+        <label htmlFor='amount'>paragraphs:</label>
         <input
           type='number'
           name='amount'
@@ -28,13 +27,11 @@ function App() {
           value={count}
           onChange={(e) => setCount(e.target.value)}
         />
-        <button type='submit' className='btn'>
-          generator
-        </button>
+        <button className='btn'>generate</button>
       </form>
       <article className='lorem-text'>
-        {texts.map((text, index) => {
-          return <p key={index}>{text}</p>;
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
         })}
       </article>
     </section>
